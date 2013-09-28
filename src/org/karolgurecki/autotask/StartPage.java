2135e956f7cb86dbe4d04b523205b7a0189a0ee1
karolgurecki <karolgurecki@gmail.com>
2013-09-28 15:34:58 +0200

    Init commit

---
+++ src/org/karolgurecki/autotask/StartPage.java
@@ -0,0 +1,15 @@
+package org.karolgurecki.autotask;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartPage extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
