372aa78e42fd9dec53a362402c3fb6c5945af8dc
Karol Górecki <k.gorecki@tt.com.pl>
2013-10-09 16:24:33 +0200

    some change

--- src/org/karolgurecki/autotask/utils/EXceptionUtils.java
+++ src/org/karolgurecki/autotask/utils/EXceptionUtils.java
@@ -4,22 +4,23 @@ package org.karolgurecki.autotask.utils;
  * Created by goreckik on 09.10.13.
  */
 public class ExceptionUtils {
-    private ExceptionUtils(){
+    private ExceptionUtils() {
 
     }
 
     /**
      * Converts stack trace to string from from given exception
+     *
      * @param e the exception
      * @return created string
      */
-    public static String stackTraceToString(Exception e){
-        StringBuilder builder=new StringBuilder();
+    public static String stackTraceToString(Exception e) {
+        StringBuilder builder = new StringBuilder();
 
         builder.append(e.getMessage());
 
-        for(StackTraceElement stackTraceElement:e.getStackTrace()){
-            builder.append("\n"+ stackTraceElement);
+        for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+            builder.append("\n" + stackTraceElement);
         }
 
         return builder.toString();
