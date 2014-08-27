ac604e5c479cef870aab9d68b58516f1872d8571
goreckik <goreckik@tt.com.pl>
2014-08-27 15:55:49 +0200

    Incoming Call Trigger

--- app/src/main/java/org/karolgurecki/autotask/tasks/triggers/IncomingCallTrigger.java
+++ app/src/main/java/org/karolgurecki/autotask/tasks/triggers/IncomingCallTrigger.java
@@ -3,7 +3,14 @@ package org.karolgurecki.autotask.tasks.triggers;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.telephony.TelephonyManager;
 
+import org.karolgurecki.autotask.R;
+import org.karolgurecki.autotask.ui.tasks.AbstractInputTextDialog;
+import org.karolgurecki.autotask.utils.ConstanceFieldHolder;
+
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,39 +21,73 @@ import java.util.Set;
  * Since: 0.01
  */
 public class IncomingCallTrigger extends AbstractBroadcastReceiverTrigger {
+
+    private static final IntentFilter INTENT_FILTER = new IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
+
+    private static final Map<String, Set<Intent>> ACTIVATED_VALUES_MAP = new HashMap<>();
+
+    private static String activeValue = "";
+
     @Override
     protected Map<Boolean, Set<Intent>> receive(Context context, Intent intent) {
-        return null;
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        Map<Boolean, Set<Intent>> response = new HashMap<>();
+        if (state != null) {
+
+            String phoneNumber=intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            if (phoneNumber != null &&
+                    state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)) {
+
+                for (Map.Entry<String, Set<Intent>> activatedValue : ACTIVATED_VALUES_MAP.entrySet()) {
+                    if (phoneNumber.contains(activatedValue.getKey())) {
+                        putValueIntoMap(response, true, activatedValue.getValue());
+                    } else {
+                        putValueIntoMap(response, false, activatedValue.getValue());
+                    }
+                }
+            }
+        }
+        return response;
+    }
+
+    private void putValueIntoMap(Map<Boolean, Set<Intent>> response, boolean key,
+                                 Set<Intent> intents) {
+        Set<Intent> intentSet = response.get(key);
+        if (intentSet != null) {
+            intentSet.addAll(intents);
+        } else {
+            response.put(key, new HashSet<>(intents));
+        }
     }
 
     @Override
     public String getDisplayName(Context context) {
-        return "Incoming Call Trigger";
+        return context.getString(R.string.incomingCallTriggerName);
     }
 
     @Override
     public String getDisplayConfiguration(Context context) {
-        return null;
+        return String.format("%s: %s", context.getString(R.string.ifCalling), activeValue);
     }
 
     @Override
     public void openDialog(Context context) {
-
+        new IncomingCallDialog(context, context.getString(R.string.incomingCalldialogTitle));
     }
 
     @Override
     public IntentFilter getIntentFilter() {
-        return null;
+        return INTENT_FILTER;
     }
 
     @Override
     public String getConfig() {
-        return null;
+        return activeValue;
     }
 
     @Override
     public void setConfig(String config) {
-
+        activeValue = config;
     }
 
     @Override
@@ -56,6 +97,29 @@ public class IncomingCallTrigger extends AbstractBroadcastReceiverTrigger {
 
     @Override
     public void assignResponseIntentToActivationStatus() {
+        Set<Intent> responseIntents = ACTIVATED_VALUES_MAP.get(activeValue);
 
+        if (responseIntents == null) {
+            responseIntents = new HashSet<>();
+            ACTIVATED_VALUES_MAP.put(activeValue, responseIntents);
+        }
+
+        responseIntents.add(responseIntent);
+    }
+
+    private class IncomingCallDialog extends AbstractInputTextDialog {
+
+        public IncomingCallDialog(Context context, String title) {
+            super(context, title, ConstanceFieldHolder.TRIGGER_TYPE);
+        }
+
+        @Override
+        protected boolean setActiveValue(String text) {
+            if (text == null || !text.matches("[+]?\\d+")) {
+                return false;
+            }
+            activeValue = text;
+            return true;
+        }
     }
 }
